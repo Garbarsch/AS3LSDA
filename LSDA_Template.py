f3e4a5519556bed91f1933be2d9ef5567da25e43
Garbarsch <75129820+Garbarsch@users.noreply.github.com>
2022-05-10 14:09:40 +0200

    Update LSDA_Template.py

--- LSDA_Template.py
+++ LSDA_Template.py
@@ -9,7 +9,7 @@ ia = InteractiveLoginAuthentication(tenant_id='bea229b6-7a08-4086-b44c-71f57f716
 from azureml.core import Workspace
 #ws = Workspace.from_config()
 #ws = Workspace.from_config()
-ws = Workspace(subscription_id = "635b8853-8742-4156-907d-5f83ad2ada58", resource_group="LSDA_group", workspace_name="LSDAML", auth=None, _location=None, _disable_service_check=False, _workspace_id=None, sku='basic', tags=None, _cloud='AzureCloud')
+ws = Workspace(subscription_id = "635b8853-8742-4156-907d-5f83ad2ada58", resource_group="LSDA_group", workspace_name="LSDAML", auth=ia, _location=None, _disable_service_check=False, _workspace_id=None, sku='basic', tags=None, _cloud='AzureCloud')
 #mlflow.set_tracking_uri(ws.get_mlflow_tracking_uri())
 #from azureml.mlflow import register_model
 experiment_name = 'experiment_with_mlflow'
