cc00b23b362b256340217786d87619ba43f9eef2
Garbarsch <75129820+Garbarsch@users.noreply.github.com>
2022-05-10 14:04:22 +0200

    Update LSDA_Template.py

--- LSDA_Template.py
+++ LSDA_Template.py
@@ -8,11 +8,11 @@ from azureml.core import Workspace
 #ws = Workspace.from_config()
 #ws = Workspace.from_config()
 ws = Workspace(subscription_id = "635b8853-8742-4156-907d-5f83ad2ada58", resource_group="LSDA_group", workspace_name="LSDAML", auth=None, _location=None, _disable_service_check=False, _workspace_id=None, sku='basic', tags=None, _cloud='AzureCloud')
-mlflow.set_tracking_uri(ws.get_mlflow_tracking_uri())
+#mlflow.set_tracking_uri(ws.get_mlflow_tracking_uri())
 #from azureml.mlflow import register_model
 experiment_name = 'experiment_with_mlflow'
-import os
-os.environ[MLFLOW_TRACKING_URI] = "azureml://japaneast.api.azureml.ms/mlflow/v1.0/subscriptions/635b8853-8742-4156-907d-5f83ad2ada58/resourceGroups/LSDA_group/providers/Microsoft.MachineLearningServices/workspaces/LSDAML"
+#import os
+#os.environ[MLFLOW_TRACKING_URI] = "azureml://japaneast.api.azureml.ms/mlflow/v1.0/subscriptions/635b8853-8742-4156-907d-5f83ad2ada58/resourceGroups/LSDA_group/providers/Microsoft.MachineLearningServices/workspaces/LSDAML"
 
 mlflow.set_tracking_uri("azureml://japaneast.api.azureml.ms/mlflow/v1.0/subscriptions/635b8853-8742-4156-907d-5f83ad2ada58/resourceGroups/LSDA_group/providers/Microsoft.MachineLearningServices/workspaces/LSDAML")
 
